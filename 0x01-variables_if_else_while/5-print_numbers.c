c7fd1dadce5149e6d88d92e00ea1936a73c84146
Omar <omar.yassermarei@live.com>
2023-06-16 18:21:49 -0700

    Added 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints integers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 123456789;
+	printf("%d", 0);
+	printf("%d\n", n);
+	return (0);
+}
