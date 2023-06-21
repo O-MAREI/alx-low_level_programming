a0d098ba5ca84190188735f04f47434fdb9671b4
Omar <omar.yassermarei@live.com>
2023-06-20 17:28:16 -0700

    Aded function to header. Adjusted 1-alphabet again.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * main - Prints alphabet in lowercase.
- * Return: On success 0.
+ * print_alphabet - Prints alphabet in lowercase.
  */
-int main(void)
+void print_alphabet(void)
 {
 	char n;
 	for (n = 'a'; n <= 'z'; n++)
 		_putchar(n);
 	_putchar('\n');
-	return (0);
 }
