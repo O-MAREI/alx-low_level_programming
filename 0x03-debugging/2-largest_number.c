b3f5fe3ef92645fb127e252e9cfffbf925c95a98
Omar <omar.yassermarei@live.com>
2023-06-23 18:13:45 -0700

    Added additional required conditionals to 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,6 +12,42 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
+    if (a == b)
+    {
+	    if (a > c)
+	    {
+		    return (a);
+	    }
+	    else
+	    {
+		    return (c);
+	    }
+    }
+
+    if (a == c)
+    {
+	    if (a > b)
+	    {
+		    return (a);
+	    }
+	    else
+	    {
+		    return (b);
+	    }
+    }
+
+    if (b == c)
+    {
+	    if (b > a)
+	    {
+		    return (b);
+	    }
+	    else
+	    {
+		    return (a);
+	    }
+    }
+
     if (a > b && a > c)
     {
         largest = a;
