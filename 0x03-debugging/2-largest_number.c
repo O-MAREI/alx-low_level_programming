cf9b6ef3d6291287f29d2668af9df4e220b5f916
Omar <omar.yassermarei@live.com>
2023-06-23 18:41:19 -0700

    Tryong to fix that damn error 12

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,62 +1,32 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-
+* largest_number - returns the largest of 3 numbers
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
+	
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-    if (a == b)
-    {
-	    if (a > c)
-	    {
-		    return (a);
-	    }
-	    else
-	    {
-		    return (c);
-	    }
-    }
-    if (a == c)
-    {
-	    if (a > b)
-	    {
-		    return (a);
-	    }
-	    else
-	    {
-		    return (b);
-	    }
-    }
-    if (b == c)
-    {
-	    if (b > a)
-	    {
-		    return (b);
-	    }
-	    else
-	    {
-		    return (a);
-	    }
-    }
-    if (a > b && a > c)
-    {
-	    largest = a;
-    }
-    else if (b > a && b > c)
-    {
-	    largest = b;
-    }
-    else
-    {
-	    largest = c;
-    }
-
-    return (largest);
+	return (largest);
 }
