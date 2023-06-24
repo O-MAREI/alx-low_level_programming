1aad950199374a3c86fb68c12a528418a2a52e90
Omar <omar.yassermarei@live.com>
2023-06-23 18:20:37 -0700

    Adjusted 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,56 +10,23 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
 
-    if (a == b)
-    {
-	    if (a > c)
-	    {
-		    return (a);
-	    }
-	    else
-	    {
-		    return (c);
-	    }
-    }
-
-    if (a == c)
-    {
-	    if (a > b)
-	    {
-		    return (a);
-	    }
-	    else
-	    {
-		    return (b);
-	    }
-    }
-
-    if (b == c)
-    {
-	    if (b > a)
-	    {
-		    return (b);
-	    }
-	    else
-	    {
-		    return (a);
-	    }
-    }
-
-    if (a > b && a > c)
-    {
-	    largest = a;
-    }
-    else if (b > a && b > c)
-    {
-	    largest = b;
-    }
-    else
-    {
-	    largest = c;
-    }
-
-    return (largest);
+	int largest;
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
 }
