b801a58fb4110c5280ac9e5041398ad44f4368bf
Omar <omar.yassermarei@live.com>
2023-07-17 18:30:59 -0700

    Adjusting

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,11 +13,9 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		d->name = "(nil)";
-	if (d->age == NULL)
-		d->age = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 
 
-	printf("Name: %x\nAge: %b\nOwner: %x\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
