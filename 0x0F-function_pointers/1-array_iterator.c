a5af582e09cf56a16068aea881facc985319fec2
Omar <omar.yassermarei@live.com>
2023-07-19 02:22:51 -0700

    Adjusting task 0 and 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,22 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
-void array_iterator(int *array, size_t size, void (*action)(int)){
-
+/**
+ * array_iterator - Prints every item in an array.
+ * @array: The array.
+ * @size: The size of the array.
+ * @action: Pointer to the function to be used.
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
 	int i;
 
 	if (array == NULL || action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
+	{
 		action(array[i]);
+	}
 }
