cb0c6bf38fe05c4a05392871cc77d0225fa1a200
Omar <omar.yassermarei@live.com>
2023-07-19 12:53:30 -0700

    Adjusted header, prototypes, added num1 and num2 assignments in main. Please work I'm begging on my hands and knees.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,7 +12,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int num, num2;
+	int num1, num2;
 	char *op;
 
 	if (argc != 4)
@@ -20,6 +20,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
 	if (get_op_func(op) == NULL)
 	{
