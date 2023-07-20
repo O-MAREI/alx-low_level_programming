388a608566704a05f4dba39bc6e59e2334482455
Omar <omar.yassermarei@live.com>
2023-07-20 13:26:06 -0700

    Adjusted printing,

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,14 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-	va_start(ap, arg1);
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
 
 		if (separator != NULL && i != (n - 1))
-			printf(separator);
+			printf("%f", separator);
 	}
 
 	printf("\n");
