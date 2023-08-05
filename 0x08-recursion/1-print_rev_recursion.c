2fe57737f68854f94de3ea17063949552af5c8d8
Omar <omar.yassermarei@live.com>
2023-08-05 02:34:40 -0700

    Adjusting task 1.

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,11 +6,12 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	int size = strlen(s);
+
+	if (*s)
 	{
-		_putchar(s[strlen(s - 1)]);
-		s[strlen(s - 1)] = '\0';
-		_print_rev_recursion(s);
+		_print_rev_recursion(s+1);
+		_putchar(*s);
 	}
 	else
 	{
