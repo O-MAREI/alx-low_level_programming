b7f0b861a39e97fd2534949fb8c0385a096f6fef
Omar <omar.yassermarei@live.com>
2023-08-05 01:57:37 -0700

    Working on 0x08 - Recursion. Task 0 added.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Replicates _puts
+ * @s:The string to print.
+ */
+void _puts_recursion(char *s)
+{
+	if (s != "\0")
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
